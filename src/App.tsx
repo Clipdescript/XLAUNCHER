abf67787c814b34fae8781a65384419d51413a4b
matteosicard-arch <matteosicard@icloud.com>
2026-03-08 10:14:14 +0000

    UI: Update sidebar icons (Skin & Cape)

--- src/App.tsx
+++ src/App.tsx
@@ -236,8 +236,8 @@ const App: React.FC = () => {
           
           <button className="sidebar-btn"><i className="fas fa-play"></i> COMMENT JOUER ?</button>
           <button className="sidebar-btn"><i className="fas fa-question"></i> AIDE</button>
-          <button className="sidebar-btn"><i className="fas fa-star"></i> INSTALLER SKIN</button>
-          <button className="sidebar-btn"><i className="fas fa-star"></i> CAPES ANIMÉES</button>
+          <button className="sidebar-btn"><i className="fas fa-tshirt"></i> INSTALLER SKIN</button>
+          <button className="sidebar-btn"><i className="fas fa-user-secret"></i> CAPES ANIMÉES</button>
           
           <button className="sidebar-btn orange">XL | PRISM-LAUNCHER.ORG</button>
           <button className="sidebar-btn blue">FB | PAGE FACEBOOK</button>
